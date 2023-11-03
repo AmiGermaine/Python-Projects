0b56be958850ffaa9bf1a9837b7c8f04bfd54fea
Amida <143059696+AmiGermaine@users.noreply.github.com>
2023-11-03 13:18:13 -0700

    assignment

--- file.py
+++ file.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 conn = sqlite3.connect('file.db')
-
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_file(ID INTEGER PRIMARY KEY AUTOINCREMENT, \
